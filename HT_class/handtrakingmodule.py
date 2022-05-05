528932da79e584554d81e2d80922317d85050bbd
Paha228 <102708959+nkitosibiv@users.noreply.github.com>
2022-05-05 10:42:34 +0700

    Update handtrakingmodule.py
    chetverg

--- HT_class/handtrakingmodule.py
+++ HT_class/handtrakingmodule.py
@@ -9,7 +9,8 @@ class handDetector():
         self.mpDraw = mp.solutions.drawing_utils  # утилита для рисования
         self.fingertips = [4, 8, 12, 16, 20] # кончики пальцев
         self.handList = {}
-    def findHands(self, img, draw=True):
+        self.fingers = {}
+    def findHands(self, img, draw=False):
         RGB_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # BGR -> RGB
         RGB_image.flags.writeable = False
         self.result = self.hands.process(RGB_image)  # ищем руки
@@ -43,6 +44,32 @@ class handDetector():
             if draw:
                 offset = 20
                 cv2.rectangle(img, (xmin - offset, ymin - offset), (xmax + offset, ymax + offset), (0, 255, 0), 2)
+    
+    def fingersUp(self, hundNumber=0):
+        if self.result.multi_hand_landmarks:
+            if len(self.result.multi_hand_landmarks) > hundNumber:
+                side_thumb = 'left' # с какой строны находится большой палец
+                if self.handList[hundNumber][17][0] < self.handList[hundNumber][5][0]:
+                    side_thumb = 'right'
+                self.fingers[hundNumber] = []
+                if side_thumb == 'left':
+
+                        if self.handList[hundNumber][self.fingertips[0]][0] < self.handList[hundNumber][self.fingertips[1]][0]:
+                            self.fingers[hundNumber].append(1)
+                        else:
+                             self.fingers[hundNumber].append(0)
+                            
+                else:
+                    if self.handList[hundNumber][self.fingertips[0]][0] > self.handList[hundNumber][self.fingertips[1]][0]:
+                             self.fingers[hundNumber].append(1)
+                    else:
+                        self.fingers[hundNumber].append(0)
+                for i in range(1,5):
+                    if self.handList[hundNumber][self.fingertips[i]][1] < self.handList[hundNumber][self.fingertips[i]-2][1]:
+                        self.fingers[hundNumber].append(1)
+                    else: 
+                        self.fingers[hundNumber].append(0)
+                print(self.fingers[hundNumber])
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -54,18 +81,18 @@ def main():
             continue  # переход к ближайшему циклу (while)
         
         image = cv2.flip(image, 1)  # зеркальное отражение картинки
-        image = detector.findHands(image)
+        image = detector.findHands(image, True)
         countHands = 0
         if detector.result.multi_hand_landmarks:
             countHands = len(detector.result.multi_hand_landmarks)
         for i in range(countHands):
-            detector.findPosition(image, i)
+            detector.findPosition(image, i, True )
+            detector.fingersUp(i)
         cv2.imshow("Image", image)
         if cv2.waitKey(1) &  0xFF == 27:  # esc
             break
 
 main()
-
 
 
         
